8fe1214fbd00cc99502c846506a87ed1d046501b
gorco <antcal01@ucm.es>
2017-01-13 20:30:39 +0100

    Adde wake up animation

---
+++ Assets/Scripts/Mobile/AlarmSelector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmSelector : MonoBehaviour {
+
+	private float yPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	void OnMouseDown()
+	{
+		yPosition = gameObject.transform.position.y;
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+		transform.position = new Vector3 (cursorPosition.x, yPosition, cursorPoint.z);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Estoy tocando");
+	}
+
+}
---
+++ Assets/Scripts/Mobile/Mobile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobile : MonoBehaviour {
+
+	public AnimationCurve curve;
+
+	public float animationSeconds = 1;
+
+	private Vector2 start;
+	private float delta;
+	public Vector2 goal;
+
+	public float delay;
+
+	// Use this for initialization
+	void Start()
+	{
+		delta = 0;
+		start = this.GetComponent<Transform>().position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (delta > delay)
+			this.GetComponent<Transform>().position = Vector3.Lerp(start, goal, curve.Evaluate((delta-delay) / animationSeconds));
+
+		delta += Time.deltaTime;
+	}
+}
