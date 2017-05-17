590915ece5850e65fb23ab9a6a0146ade2b3223a
gorco <antcal01@ucm.es>
2017-05-17 20:47:12 +0200

    Day4 morning finished

--- Assets/Scripts/States/Day4Part3State.cs
+++ Assets/Scripts/States/Day4Part3State.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Day4Part3State : MonoBehaviour {
+public class Day4Part3State : IState {
+
+	public Camera sceneCamera;
+	public float minScroll = 0;
+	public float maxScroll;
+	public float x;
+	public float y;
 
 	// Use this for initialization
 	void Start () {
-		
+		if(GlobalState.SharedPassQuest == 0)
+		{
+			InitMobileGUI.InitMobileGUIObject(false);
+		} else
+		{
+			InitMobileGUI.InitMobileGUIObject(true);
+			CameraScroll cam = sceneCamera.GetComponent<CameraScroll>();
+			cam.scroll_min = minScroll;
+			cam.scroll_max = maxScroll;
+			sceneCamera.transform.localPosition = new Vector3(x, y, sceneCamera.transform.localPosition.z);
+		}
 	}
 	
 	// Update is called once per frame
