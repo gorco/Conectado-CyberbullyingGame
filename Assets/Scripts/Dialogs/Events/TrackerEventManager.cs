7132a6fec6122872f15e63429c384f41534d7a8a
gorco <antcal01@ucm.es>
2017-05-29 20:30:48 +0200

    Changed second nightmare and added options tracker

---
+++ Assets/Scripts/Dialogs/Events/TrackerEventManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Isometra;
+using System;
+using Isometra.Sequences;
+using RAGE.Analytics;
+using RAGE.Analytics.Formats;
+
+public class TrackerEventManager : EventManager {
+
+	public override void ReceiveEvent(IGameEvent ev)
+	{
+		switch (ev.Name)
+		{
+			case "event finished":
+				var finished = ev.getParameter("event") as IGameEvent;
+				switch (finished.Name)
+				{
+					case "show dialog options":
+						var optionsName = finished.getParameter("name") as string;
+						var optionsQuestion = finished.getParameter("message") as string;
+						var optionList = finished.getParameter("options") as List<Option>;
+						var optionchosen = (int)ev.getParameter("option");
+						var response = optionList[optionchosen];
+
+						Tracker.T.alternative.Selected(optionsName, response.Text, AlternativeTracker.Alternative.Dialog);
+
+						break;
+				}
+
+				break;
+		}
+	}
+
+	public override void Tick()
+	{
+		throw new NotImplementedException();
+	}
+
+}
